707d069703b9c6f3e8b40a275df5654671ae79db
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-02-12 14:36:06 -0600

    updated js code

--- assets/script.js
+++ assets/script.js
@@ -44,7 +44,7 @@ function generatePassword() {
 
 if (!includeCharachters && !includeLower && !includeUpper && !IncludeNumbers) {
   // alert(" you must include one charachter type")
-  alert("your password should include one charachter type,lowercase or upper case and number");
+  alert("your password should include one charachter type or lowercase or upper case or number");
   return "";
 }
  
